e6563a0dab2f7406ef8dbfc11df288bb4fc5698d
olliekampo <olliekampo@gmail.com>
2024-06-30 14:50:13 +0100

    Tidy up and fixes.

--- sweep_coverage_path_planning.py
+++ sweep_coverage_path_planning.py
@@ -27,43 +27,9 @@ do_animation = True
 
 def rot_mat_2d(angle):
     """
-    Create 2D rotation matrix from an angle
-
-    Parameters
-    ----------
-    angle :
-
-    Returns
-    -------
-    A 2D rotation matrix
-
-    Examples
-    --------
-    >>> angle_mod(-4.0)
-
-
+    Create 2D rotation matrix from an angle.
     """
     return Rot.from_euler('z', angle).as_matrix()[0:2, 0:2]
-
-
-@total_ordering
-class FloatGrid:
-
-    def __init__(self, init_val=0.0):
-        self.data = init_val
-
-    def get_float_data(self):
-        return self.data
-
-    def __eq__(self, other):
-        if not isinstance(other, FloatGrid):
-            return NotImplemented
-        return self.get_float_data() == other.get_float_data()
-
-    def __lt__(self, other):
-        if not isinstance(other, FloatGrid):
-            return NotImplemented
-        return self.get_float_data() < other.get_float_data()
 
 
 class GridMap:
@@ -71,10 +37,16 @@ class GridMap:
     GridMap class
     """
 
-    def __init__(self, width, height, resolution,
-                 center_x, center_y, init_val=FloatGrid(0.0)):
-        """__init__
-
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        resolution: float,
+        center_x: float,
+        center_y: float,
+        init_val: float = 0.0
+    ) -> None:
+        """
         :param width: number of grid for width
         :param height: number of grid for height
         :param resolution: grid resolution [m]
@@ -84,6 +56,8 @@ class GridMap:
         """
         self.width = width
         self.height = height
+        self.data = [[init_val] * self.height for _ in range(self.width)]
+
         self.resolution = resolution
         self.center_x = center_x
         self.center_y = center_y
@@ -91,40 +65,63 @@ class GridMap:
         self.left_lower_x = self.center_x - self.width / 2.0 * self.resolution
         self.left_lower_y = self.center_y - self.height / 2.0 * self.resolution
 
-        self.n_data = self.width * self.height
-        self.data = [init_val] * self.n_data
-        self.data_type = type(init_val)
-
     def get_value_from_xy_index(self, x_ind, y_ind):
-        """get_value_from_xy_index
-
-        when the index is out of grid map area, return None
-
-        :param x_ind: x index
-        :param y_ind: y index
-        """
-
-        grid_ind = self.calc_grid_index_from_xy_index(x_ind, y_ind)
-
-        if 0 <= grid_ind < self.n_data:
-            return self.data[grid_ind]
+        if 0 <= x_ind < self.width and 0 <= y_ind < self.height:
+            return self.data[x_ind][y_ind]
         else:
             return None
 
     def get_xy_index_from_xy_pos(self, x_pos, y_pos):
-        """get_xy_index_from_xy_pos
-
-        :param x_pos: x position [m]
-        :param y_pos: y position [m]
-        """
-        x_ind = self.calc_xy_index_from_position(
-            x_pos, self.left_lower_x, self.width)
-        y_ind = self.calc_xy_index_from_position(
-            y_pos, self.left_lower_y, self.height)
+        x_ind = self.__index_from_pos(x_pos, self.left_lower_x, self.width)
+        y_ind = self.__index_from_pos(y_pos, self.left_lower_y, self.height)
 
         return x_ind, y_ind
 
-    def set_value_from_xy_pos(self, x_pos, y_pos, val):
+    def __index_from_pos(self, pos, lower_pos, max_index):
+        ind = int(np.floor((pos - lower_pos) / self.resolution))
+        if 0 <= ind <= max_index:
+            return ind
+        else:
+            return None
+
+    def get_xy_pos_from_xy_index(self, x_ind, y_ind):
+        x_pos = self.__pos_from_index(x_ind, self.left_lower_x)
+        y_pos = self.__pos_from_index(y_ind, self.left_lower_y)
+
+        return x_pos, y_pos
+
+    def __pos_from_index(self, index, lower_pos):
+        return lower_pos + index * self.resolution + self.resolution / 2.0
+
+    def check_occupied_from_xy_index(self, x_ind, y_ind, occupied_val):
+
+        val = self.get_value_from_xy_index(x_ind, y_ind)
+
+        if val is None or val >= occupied_val:
+            return True
+        else:
+            return False
+
+    def set_value_from_xy_index(self, x_ind, y_ind, val):
+        """set_value_from_xy_index
+
+        return bool flag, which means setting value is succeeded or not
+
+        :param x_ind: x index
+        :param y_ind: y index
+        :param val: grid value
+        """
+
+        if (x_ind is None) or (y_ind is None):
+            return False, False
+
+        if 0 <= x_ind < self.width and 0 <= y_ind < self.height:
+            self.data[x_ind][y_ind] = val
+            return True
+        else:
+            return False
+
+    def set_value_from_xy_pos(self, x_pos, y_pos, val):  # TODO
         """set_value_from_xy_pos
 
         return bool flag, which means setting value is succeeded or not
@@ -143,27 +140,6 @@ class GridMap:
 
         return flag
 
-    def set_value_from_xy_index(self, x_ind, y_ind, val):
-        """set_value_from_xy_index
-
-        return bool flag, which means setting value is succeeded or not
-
-        :param x_ind: x index
-        :param y_ind: y index
-        :param val: grid value
-        """
-
-        if (x_ind is None) or (y_ind is None):
-            return False, False
-
-        grid_ind = int(y_ind * self.width + x_ind)
-
-        if 0 <= grid_ind < self.n_data and isinstance(val, self.data_type):
-            self.data[grid_ind] = val
-            return True  # OK
-        else:
-            return False  # NG
-
     def set_value_from_polygon(self, pol_x, pol_y, val, inside=True):
         """set_value_from_polygon
 
@@ -180,74 +156,30 @@ class GridMap:
             np.append(pol_x, pol_x[0])
             np.append(pol_y, pol_y[0])
 
+        set_ = 0
         # setting value for all grid
         for x_ind in range(self.width):
             for y_ind in range(self.height):
-                x_pos, y_pos = self.calc_grid_central_xy_position_from_xy_index(
+                x_pos, y_pos = self.get_xy_pos_from_xy_index(
                     x_ind, y_ind)
 
                 flag = self.check_inside_polygon(x_pos, y_pos, pol_x, pol_y)
 
                 if flag is inside:
                     self.set_value_from_xy_index(x_ind, y_ind, val)
+                    set_ += 1
 
-    def calc_grid_index_from_xy_index(self, x_ind, y_ind):
-        grid_ind = int(y_ind * self.width + x_ind)
-        return grid_ind
-
-    def calc_xy_index_from_grid_index(self, grid_ind):
-        y_ind, x_ind = divmod(grid_ind, self.width)
-        return x_ind, y_ind
-
-    def calc_grid_index_from_xy_pos(self, x_pos, y_pos):
-        """get_xy_index_from_xy_pos
-
-        :param x_pos: x position [m]
-        :param y_pos: y position [m]
-        """
-        x_ind = self.calc_xy_index_from_position(
-            x_pos, self.left_lower_x, self.width)
-        y_ind = self.calc_xy_index_from_position(
-            y_pos, self.left_lower_y, self.height)
-
-        return self.calc_grid_index_from_xy_index(x_ind, y_ind)
-
-    def calc_grid_central_xy_position_from_grid_index(self, grid_ind):
-        x_ind, y_ind = self.calc_xy_index_from_grid_index(grid_ind)
-        return self.calc_grid_central_xy_position_from_xy_index(x_ind, y_ind)
-
-    def calc_grid_central_xy_position_from_xy_index(self, x_ind, y_ind):
-        x_pos = self.calc_grid_central_xy_position_from_index(
-            x_ind, self.left_lower_x)
-        y_pos = self.calc_grid_central_xy_position_from_index(
-            y_ind, self.left_lower_y)
-
-        return x_pos, y_pos
-
-    def calc_grid_central_xy_position_from_index(self, index, lower_pos):
-        return lower_pos + index * self.resolution + self.resolution / 2.0
-
-    def calc_xy_index_from_position(self, pos, lower_pos, max_index):
-        ind = int(np.floor((pos - lower_pos) / self.resolution))
-        if 0 <= ind <= max_index:
-            return ind
-        else:
-            return None
-
-    def check_occupied_from_xy_index(self, x_ind, y_ind, occupied_val):
-
-        val = self.get_value_from_xy_index(x_ind, y_ind)
-
-        if val is None or val >= occupied_val:
-            return True
-        else:
-            return False
-
-    def expand_grid(self, occupied_val=FloatGrid(1.0)):
+    def pad_obstacles(self, occupied_val=1.0):
+        """Pad one cell around obstacles with a value greater than or equal to `occupied_val`."""
+        # NOTE: All this does is add a padding of one cell around the obstacles...
+        # And it doesn't even do the top left of bottom right corners...
         x_inds, y_inds, values = [], [], []
 
         for ix in range(self.width):
             for iy in range(self.height):
+                # NOTE: occupied_val is only ever set to 1.0, so this is just
+                # checking for cells with a value of 1.0, and then padding 1 cell
+                # around them with 1.0s...
                 if self.check_occupied_from_xy_index(ix, iy, occupied_val):
                     x_inds.append(ix)
                     y_inds.append(iy)
@@ -290,11 +222,9 @@ class GridMap:
         print("center_y:", self.center_y)
         print("left_lower_x:", self.left_lower_x)
         print("left_lower_y:", self.left_lower_y)
-        print("n_data:", self.n_data)
 
     def plot_grid_map(self, ax=None):
-        float_data_array = np.array([d.get_float_data() for d in self.data])
-        grid_data = np.reshape(float_data_array, (self.height, self.width))
+        float_data_array = np.array(self.data)
         if not ax:
             fig, ax = plt.subplots()
         heat_map = ax.pcolor(grid_data, cmap="Blues", vmin=0.0, vmax=1.0)
@@ -303,7 +233,7 @@ class GridMap:
         return heat_map
 
 
-def check_occupied(c_x_index, c_y_index, grid_map, occupied_val=FloatGrid(0.5)):
+def check_occupied(c_x_index: int, c_y_index: int, grid_map: GridMap, occupied_val: float = 0.5):
     return grid_map.check_occupied_from_xy_index(c_x_index, c_y_index, occupied_val)
 
 
@@ -325,7 +255,7 @@ class SweepSearcher:
     ) -> None:
         self.moving_direction = moving_direction
         self.sweep_direction = sweep_direction
-        self.turning_window: list[tuple[float, float]] = []
+        self.turning_window: list[tuple[int, int]] = []
         self.update_turning_window()
         self.x_indexes_goal_y = x_inds_goal_y
         self.goal_y = goal_y
@@ -374,7 +304,7 @@ class SweepSearcher:
                 # moving backward
                 next_c_x_index = -self.moving_direction + c_x_index
                 next_c_y_index = c_y_index
-                if check_occupied(next_c_x_index, next_c_y_index, grid_map, FloatGrid(1.0)):
+                if check_occupied(next_c_x_index, next_c_y_index, grid_map, 1.0):
                     # moved backward, but the grid is occupied by obstacle
                     return None, None
 
@@ -391,7 +321,7 @@ class SweepSearcher:
 
             return next_c_x_index, next_c_y_index
 
-    def find_safe_turning_grid(self, c_x_index, c_y_index, grid_map):
+    def find_safe_turning_grid(self, c_x_index: int, c_y_index: int, grid_map: GridMap) -> tuple[int, int] | tuple[None, None]:
         """
         This method could be used in a path planning algorithm to find a safe
         grid cell to turn into when the current moving direction is blocked by
@@ -408,7 +338,7 @@ class SweepSearcher:
         # directions to turn into. The relative coordinates are added to the
         # current coordinates to get the absolute coordinates of the turning
         # grid.
-        for (d_x_ind, d_y_ind) in self.turning_window:
+        for (d_x_ind, d_y_ind) in self.turning_window:  # TODO: Turning window is a mixture of ints and int enum items!?!?
 
             # For each cell in the turning window, it calculates the absolute
             # coordinates (next_x_ind, next_y_ind) by adding the relative
@@ -437,8 +367,9 @@ class SweepSearcher:
     def update_turning_window(self):
         # turning window definition
         # robot can move grid based on it.
+        # TODO: Using int enums like this is just horrible...
         self.turning_window = [
-            (self.moving_direction, 0.0),
+            (self.moving_direction, 0),
             (self.moving_direction, self.sweep_direction),
             (0, self.sweep_direction),
             (-self.moving_direction, self.sweep_direction),
@@ -448,7 +379,7 @@ class SweepSearcher:
         self.moving_direction *= -1
         self.update_turning_window()
 
-    def search_start_grid(self, grid_map):
+    def search_start_grid(self, grid_map: GridMap) -> tuple[int, int]:
         x_inds = []
         y_ind = 0
         if self.sweep_direction == self.SweepDirection.DOWN:
@@ -593,8 +524,11 @@ def setup_grid_map(ox, oy, resolution, sweep_direction, offset_grid=10):
 
     grid_map = GridMap(width, height, resolution, center_x, center_y)
     grid_map.print_grid_map_info()
-    grid_map.set_value_from_polygon(ox, oy, FloatGrid(1.0), inside=False)
-    grid_map.expand_grid()
+    print("Initial free cells: ", sum([x.count(0.0) for x in grid_map.data]))
+    grid_map.set_value_from_polygon(ox, oy, 1.0, inside=False)
+    print("Free cells after adding walls: ", sum([x.count(0.0) for x in grid_map.data]))
+    grid_map.pad_obstacles()
+    print("Free cells after padding walls: ", sum([x.count(0.0) for x in grid_map.data]))
 
     x_inds_goal_y: list[int] = []
     goal_y: int = 0
@@ -618,32 +552,30 @@ def sweep_path_search(
 ) -> tuple[list[float], list[float]]:
     # search start grid
     c_x_index, c_y_index = sweep_searcher.search_start_grid(grid_map)
+    print("Start grid indices: ", c_x_index, c_y_index)
 
-    if not grid_map.set_value_from_xy_index(c_x_index, c_y_index, FloatGrid(0.5)):
+    if not grid_map.set_value_from_xy_index(c_x_index, c_y_index, 0.5):
         print("Cannot find start grid")
         return [], []
 
-    x, y = grid_map.calc_grid_central_xy_position_from_xy_index(c_x_index,
-                                                                c_y_index)
+    x, y = grid_map.get_xy_pos_from_xy_index(c_x_index, c_y_index)
     px = [x]
     py = [y]
 
     while True:
-        c_x_index, c_y_index = sweep_searcher.move_target_grid(c_x_index,
-                                                               c_y_index,
-                                                               grid_map)
+        c_x_index, c_y_index = sweep_searcher.move_target_grid(c_x_index, c_y_index, grid_map)
 
         if (sweep_searcher.is_search_done(grid_map)
                 or (c_x_index is None or c_y_index is None)):
             break
 
-        x, y = grid_map.calc_grid_central_xy_position_from_xy_index(
+        x, y = grid_map.get_xy_pos_from_xy_index(
             c_x_index, c_y_index)
 
         px.append(x)
         py.append(y)
 
-        grid_map.set_value_from_xy_index(c_x_index, c_y_index, FloatGrid(0.5))
+        grid_map.set_value_from_xy_index(c_x_index, c_y_index, 0.5)
 
     return px, py
 
